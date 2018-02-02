6962464335f0edf9b5ddc405719f46fb68529175
Isaac <31518729+Ikegorgon@users.noreply.github.com>
2018-02-02 13:34:12 -0700

    Added BlackackRunner

---
+++ src/blackjack/controller/BlackjackRunner.java
@@ -0,0 +1,8 @@
+package blackjack.controller;
+
+public class BlackjackRunner {
+	public static void main (String [ ] args) {
+		BlackjackController app = new BlackjackController();
+		app.start();
+	}
+}
