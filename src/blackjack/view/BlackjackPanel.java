33a7351885de23e8f95eaafc7c13ff723dd92c5a
Isaac <31518729+Ikegorgon@users.noreply.github.com>
2018-02-05 13:26:16 -0700

    Added base code to Panel

--- src/blackjack/view/BlackjackPanel.java
+++ src/blackjack/view/BlackjackPanel.java
@@ -1,5 +1,35 @@
 package blackjack.view;
 
-public class BlackjackPanel {
+import blackjack.controller.*;
+import javax.swing.*;
+import java.util.*;
+import java.awt.*;
+import java.awt.event.*;
 
+public class BlackjackPanel extends JPanel {
+	private BlackjackController app;
+	private BlackjackFrame frame;
+	private SpringLayout appLayout;
+	private JLabel a;
+	public BlackjackPanel(BlackjackController app) {
+		super();
+		this.app = app;
+		frame = new BlackjackFrame();
+		appLayout = new SpringLayout();
+		
+		setupPanel();
+		setupLayout();
+		setupListeners();
+	}
+	private void setupPanel() {
+		this.setBackground(Color.GREEN.darker());
+		this.setLayout(appLayout);
+//		this.add();
+	}
+	private void setupLayout() {
+		
+	}
+	private void setupListeners() {
+		
+	}
 }
