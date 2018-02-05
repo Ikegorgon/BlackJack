dcf6e76029236d85ce6f916db4068b66a20b872e
Isaac <31518729+Ikegorgon@users.noreply.github.com>
2018-02-05 13:26:29 -0700

    Added to Controller

--- src/blackjack/controller/BlackjackController.java
+++ src/blackjack/controller/BlackjackController.java
@@ -1,7 +1,19 @@
 package blackjack.controller;
 
+import blackjack.view.*;
+
 public class BlackjackController {
+	BlackjackFrame appFrame = new BlackjackFrame(this);
 	void start() {
-		
+
+	}
+	
+	
+	
+	
+	
+	
+	public BlackjackFrame getFrame() {
+		return appFrame;
 	}
 }
