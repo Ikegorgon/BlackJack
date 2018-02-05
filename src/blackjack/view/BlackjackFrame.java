006afff14c605363482ae4055465e26f503eb5bc
Isaac <31518729+Ikegorgon@users.noreply.github.com>
2018-02-05 11:46:57 -0700

    Added code for the BlackjackFrame

--- src/blackjack/view/BlackjackFrame.java
+++ src/blackjack/view/BlackjackFrame.java
@@ -1,5 +1,29 @@
 package blackjack.view;
 
-public class BlackjackFrame {
+import blackjack.controller.*;
+import javax.swing.*;
 
+public class BlackjackFrame extends JFrame {
+	private BlackjackController app;
+	private BlackjackPanel appPanel;
+	public BlackjackFrame(BlackjackController app) {
+		super();
+		this.app = app;
+		appPanel = new BlackjackPanel(app);
+		setupFrame();
+	}
+	public BlackjackFrame() {
+		
+	}
+	private void setupFrame() {
+		this.setContentPane(appPanel);
+		this.setTitle("Blackjack");
+		this.setSize(500, 500);
+		this.setResizable(false);
+		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
+		this.setVisible(true);
+	}
+	public BlackjackController getController() {
+		return app;
+	}
 }
