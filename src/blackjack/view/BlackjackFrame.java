042adb6fb758042589aea1eda618fe712a62f867
Isaac <31518729+Ikegorgon@users.noreply.github.com>
2018-02-05 13:26:02 -0700

    Changed size of app

--- src/blackjack/view/BlackjackFrame.java
+++ src/blackjack/view/BlackjackFrame.java
@@ -4,6 +4,7 @@ import blackjack.controller.*;
 import javax.swing.*;
 
 public class BlackjackFrame extends JFrame {
+	private static final long serialVersionUID = 1L;
 	private BlackjackController app;
 	private BlackjackPanel appPanel;
 	public BlackjackFrame(BlackjackController app) {
@@ -18,7 +19,7 @@ public class BlackjackFrame extends JFrame {
 	private void setupFrame() {
 		this.setContentPane(appPanel);
 		this.setTitle("Blackjack");
-		this.setSize(500, 500);
+		this.setSize(750, 750);
 		this.setResizable(false);
 		this.setDefaultCloseOperation(EXIT_ON_CLOSE);
 		this.setVisible(true);
