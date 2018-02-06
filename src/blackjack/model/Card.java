07e9e95f05dcc104aa4ee7bc4b5a97f00ad1f416
Isaac <31518729+Ikegorgon@users.noreply.github.com>
2018-02-06 13:25:16 -0700

    Made Card.java

--- src/blackjack/model/Card.java
+++ src/blackjack/model/Card.java
@@ -1,5 +1,38 @@
 package blackjack.model;
 
 public class Card {
-
+	private String suite;
+	private int number;
+	private int score;
+	
+	public Card(String suite, int number) {
+		this.suite = suite;
+		this.number = number;
+		if (number > 10) {
+			this.score = 10;
+		} else {
+			this.score = number;
+		}
+	}
+	public String toFile() {
+		String description = "";
+		description += suite + number + ".png";;
+		return description;
+	}
+	
+	public String getSuite() {
+		return suite;
+	}
+	public int getNumber() {
+		return number;
+	}
+	public int getScore() {
+		return score;
+	}
+	public void setSuite(String suite) {
+		this.suite = suite;
+	}
+	public void setNumber(int number) {
+		this.number = number;
+	}
 }
